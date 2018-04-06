d53be71e4710ffd964370e9781a0ee7c47ab2f46
Asheley McDonald <asheleymcdonald@gmail.com>
2018-04-06 19:02:02 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,55 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each do |name|
+    new_array << name[2] = "$"
+  end
+end
+
+def find_a(array)
+  all_a = []
+  array.each do |name|
+    all_a << name if name[0].downcase == "a"
+  end
+  all_a
+end
+
+def sum_array(array)
+  array.inject(0) { |sum, i| sum + i }
+end
+
+def add_s(array)
+  array.each_with_index.collect do |element, index|
+    if index == 1
+      element
+    else
+      element << "s"
+    end
+  end
+end
